76bdb4e13843ef7491b890f379831fb0c5c50917
siavash mohseni <siavashmohsenimhi79@gmail.com>
2022-12-14 18:32:47 +0330

    add codes

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,38 +1,24 @@
 import React from 'react';
 import Layout from '../layout/Layout';
 import { useDispatchProducts, useProducts } from '../context/ProductContextProvider';
-import { NavLink } from 'react-router-dom';
-import trashIcon from '../assets/images/trash.svg'
+import { renderProductsInCart, checkOutInCart, clearCartInCart} from '../utilities/functions';
+import { toast } from 'react-toastify';
 import '../assets/styles/cartpage.css'
 
 const CartPage = () => {
     const { products, totalPrice, itemsCounter, checkout} = useProducts()
     const dispatch = useDispatchProducts()
 
-    const renderProductsInCart = item => {
-        return (
-            <div key={item.id} className='productItem'> 
-                <div className='productImage'>
-                    <img src={item.image} alt="img"/>
-                </div>
-                <div className="productDesc">
-                    <p>{item.name}</p>
-                    <span>$ {item.price * item.quantity}</span>
-                </div>
-                <div className='productBtnContainer'>
-                    {
-                        item.quantity > 1 ? 
-                        <button onClick={() => dispatch({type: "DECREASE", payload: item})}>-</button> 
-                        : <button onClick={() => dispatch({type: "REMOVE", payload: item})}>
-                            <img src={trashIcon} alt="icon" style={{width: "20px"}}/>
-                        </button>
-                        
-                    }
-                    <button>{item.quantity}</button>
-                    <button onClick={() => dispatch({type: "INCREASE", payload: item})}>+</button>
-                </div>
-            </div>
-        )
+    //for checkout in cart component
+    const checkOutHandler = () => {
+        dispatch({type: "CHECKOUT"})
+        toast.success('checkout is successfuly.')
+    }
+
+    //for clear cart in cart component
+    const clearCartHandler = () => {
+        dispatch({type: "CLEAR"})
+        toast.success('your cart has been cleared.')
     }
 
     return (
@@ -42,7 +28,7 @@ const CartPage = () => {
                     <section className='productItemsList'>
                         {
                             products.map(item => (
-                                renderProductsInCart(item)
+                                renderProductsInCart(item, dispatch)
                             ))
                         }
                     </section>
@@ -53,27 +39,16 @@ const CartPage = () => {
                                 <p>Total Items: <span>{itemsCounter}</span></p>
                                 <p>Total Price: <span>{totalPrice}</span></p>
                                 <div className='btns-summery'>
-                                    <button onClick={() => dispatch({type: "CHECKOUT"})}>Check Out</button>
-                                    <button onClick={() => dispatch({type: "CLEAR"})}>Clear</button>
+                                    <button onClick={() => checkOutHandler()}>Check Out</button>
+                                    <button onClick={() => clearCartHandler()}>Clear</button>
                                 </div>
                             </section>
                         }
 
-                        {
-                            checkout && 
-                            <section className='checkot'>
-                                <h3 className='checkout-title'>checkout is successfuly.....</h3>
-                                <NavLink to='/'>Back to Shop</NavLink>
-                            </section>
-                        }
+                        {checkout && checkOutInCart()}
 
-                        {
-                            itemsCounter === 0 && !checkout &&
-                            <section className='clear'>
-                                <h3>there is no any products</h3>
-                                <NavLink to='/'>Back to Shop</NavLink>
-                            </section>
-                        }
+                        {itemsCounter === 0 && !checkout && clearCartInCart()}
+
                     </section>
                 </div>
             </main>
--- src/utilities/functions.js
+++ src/utilities/functions.js
@@ -1,3 +1,7 @@
+import trashIcon from '../assets/images/trash.svg'
+import { NavLink } from 'react-router-dom'
+
+//check product in cart
 function checkInCart(products, product) {
     if(products) {
         const check = products.findIndex(c => c.id === product.id)
@@ -9,11 +13,59 @@ function checkInCart(products, product) {
     }
 } 
 
+//for calculate count of products and total price in cart
 const itemsCounterAndTotalPrice = items => {
     const itemsCounter = items.reduce((total, product) => total + product.quantity, 0);
     let totalPrice = items.reduce((total, product) => total + (product.price * product.quantity), 0).toFixed(2);
     return {itemsCounter, totalPrice}
 }
 
+//for render products in map function in cart component
+const renderProductsInCart = (item, dispatch) => {
+    return (
+        <div key={item.id} className='productItem'> 
+            <div className='productImage'>
+                <img src={item.image} alt="img"/>
+            </div>
+            <div className="productDesc">
+                <p>{item.name}</p>
+                <span>$ {item.price * item.quantity}</span>
+            </div>
+            <div className='productBtnContainer'>
+                {
+                    item.quantity > 1 ? 
+                    <button onClick={() => dispatch({type: "DECREASE", payload: item})}>-</button> 
+                    : <button onClick={() => dispatch({type: "REMOVE", payload: item})}>
+                        <img src={trashIcon} alt="icon" style={{width: "20px"}}/>
+                    </button>
+                    
+                }
+                <button>{item.quantity}</button>
+                <button onClick={() => dispatch({type: "INCREASE", payload: item})}>+</button>
+            </div>
+        </div>
+    )
+}
 
-export { checkInCart, itemsCounterAndTotalPrice }
+//for checkout in cart 
+const checkOutInCart = () => {
+    return (
+        <section className='checkot'>
+            <h3 className='checkout-title'>checkout is successfuly.....</h3>
+            <NavLink to='/'>Back to Shop</NavLink>
+        </section>
+    )
+}
+
+//for clear cart in cart component
+const clearCartInCart = () => {
+    return (
+        <section className='clear'>
+            <h3>there is no any products</h3>
+            <NavLink to='/'>Back to Shop</NavLink>
+        </section>
+    )
+}
+
+export { checkInCart, itemsCounterAndTotalPrice,
+     renderProductsInCart, checkOutInCart, clearCartInCart}
